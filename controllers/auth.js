787455de32e7cd6ebe07a89dfb596433f8d6d3b8
spazevic <spazevic@yahoo.com>
2020-05-15 09:13:26 -0700

    signup created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 let db = require('../models')
 let router = require('express').Router()
+let jwt = require('jsonwebtoken')
 
 // POST /auth/login (find and validate user; send token)
 router.post('/login', (req, res) => {
@@ -11,7 +12,32 @@ router.post('/login', (req, res) => {
 // POST to /auth/signup (create user; generate token)
 router.post('/signup', (req, res) => {
   console.log(req.body)
-  res.send('STUB POST /auth/signup')
+  db.User.findOne({ email: req.body.email })
+  .then(user => {
+    if (user) {
+      return res.status(409).send({ message: 'email in use' })
+    }
+    db.User.create(req.body)
+    .then(newUser => {
+      let token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET, {
+        expiresIn: 120
+      })
+
+      res.send({token})
+    })
+    .catch(err => {
+      console.log('error creating user', err)
+      if (err.name == 'ValidationError') {
+        res.status(412).send({ message: `validation error  ${err.message}` })
+      } else {
+        res.status(500).send({ message: 'error creating user' })
+      }
+    })
+  })
+  .catch(err => {
+    console.log('error dude in post route', err)
+    res.status(503).send({ message: 'Database or server error'})
+  })
 })
 
 // NOTE: User should be logged in to access this route
--- models/user.js
+++ models/user.js
@@ -1,5 +1,50 @@
+let bcrypt = require('bcryptjs')
 let mongoose = require('mongoose')
 
 // TODO: Create user schema
+let userSchema = new mongoose.Schema({
+	firstname: {
+		type: String,
+		required: true
+	},
+	lastname: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		minlength: 6
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 8
+	},
+	pic: String,
+	admin: {
+		type: Boolean,
+		default: false
+	}
+})
+
+userSchema.pre('save', function(next) {
+	if (this.isNew) {
+		this.password = bcrypt.hashSync(this.password, 12)
+	}
+	next()
+})
+
+userSchema.set('toJSON', {
+	transform: (doc, user) => {
+		delete user.password
+		delete user.lastname
+		delete user.__v
+		return user
+	}
+})
+
+userSchema.methods.validPassword = function (typedPassword) {
+	return bcrypt.comparesync(typedPassword, this.password)
+}
 
 // TODO: Export user model
+module.exports = mongoose.model('User', userSchema)
